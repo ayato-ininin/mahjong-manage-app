6723ebd49f353fd62376714fef34d202289b788c
ayato yamauchi <>
2023-01-14 20:22:14 +0900

    add headerコンポーネント作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeisanComponent } from './components/seisan/seisan.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SeisanComponent
+    SeisanComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
